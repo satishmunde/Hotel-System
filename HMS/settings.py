0ecb83adf257697320337164d3df1526a412c7b0
satishmunde <mr.satishmunde@gmail.com>
2024-07-23 13:47:33 +0530

    Project Setup started

--- HMS/settings.py
+++ HMS/settings.py
@@ -88,17 +88,23 @@ WSGI_APPLICATION = "HMS.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'hotel_system',
+#         'USER': 'postgres',
+#         'PASSWORD': 'Admin',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'hotel_system',
-        'USER': 'postgres',
-        'PASSWORD': 'Admin',
-        'HOST': 'localhost',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
